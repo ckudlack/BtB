dea88c24112d346c0f2fe5961d8a059952eb02f3
Chris Kudlack <thebigcheese40@gmail.com>
2012-08-29 19:39:43 -0400

    So it begins

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BtB::Application.config.secret_token = 'fdd9eefa4c15c996b7c3e0fcc6ec94f21c392a859652818a19f7ffb17a5a1e10d30cec1fd9c268a730eec45fcc7962e7b4fddcbec4220cbc02357b6a1c502575'
