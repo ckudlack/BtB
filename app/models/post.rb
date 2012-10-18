7ec9ebf95bc313036ba4350b5faaf10f499a8471
Chris Kudlack <thebigcheese40@gmail.com>
2012-10-18 19:39:28 -0400

    first post added to database

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,6 @@ class Post < ActiveRecord::Base
   
   has_many :comments, :dependent => :destroy
   has_many :tags
-
-  has_attachedfile :image
  
   accepts_nested_attributes_for :tags, :allow_destroy => :true,
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
